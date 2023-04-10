1e72e66bf4b1e13a5e7686afc2fa4cf8c4aff81d
tran3jm <tran3jm@dukes.jmu.edu>
2023-04-10 11:23:24 -0400

    Game setup

--- src/lumbearjack/LumbearjackGame.java
+++ src/lumbearjack/LumbearjackGame.java
@@ -1,6 +1,38 @@
 package lumbearjack;
 
-public class LumbearjackGame
-{
+import java.io.IOException;
 
+import app.JApplication;
+import screens.StartingScreen;
+
+public class LumbearjackGame extends JApplication
+{
+  StartingScreen startScreen;
+  
+  
+  public LumbearjackGame(int width, int height)
+  {
+    super(width, height);
+    this.startScreen = new StartingScreen(width, height);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void init()
+  {
+    // TODO Auto-generated method stub
+    (JPanel)this.getContentPane().add(this.startScreen);
+  }
+  
+    /**
+   * Construct and invoke  (in the event dispatch thread) 
+   * an instance of this JApplication.
+   * 
+   * @param args The command line arguments
+   */
+  public static void main(final String[] args)
+  {
+    JApplication app = new LumbearJackGame(600, 800);
+    invokeInEventDispatchThread(app);
+  }
 }
--- src/screens/StartingScreen.java
+++ src/screens/StartingScreen.java
@@ -1,20 +1,79 @@
 package screens;
 
-import app.JApplication;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
-public class StartingScreen extends JApplication
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import app.JApplication;
+import io.ResourceFinder;
+import resources.Marker;
+
+public class StartingScreen extends JPanel implements ActionListener
 {
+  
+  private static final long serialVersionUID = 1L;
+  /**
+   * Attributes:
+   *  -start button
+   *  -resource finder
+   *  -background image
+   *  -danny logo
+   */
+  
+
+  private ResourceFinder rf;
+  
 
   public StartingScreen(int width, int height)
   {
-    super(width, height);
-    // TODO Auto-generated constructor stub
+    super();
+    this.setSize(600, 800);
+    this.rf = ResourceFinder.createInstance(new Marker());
+    
+  }
+
+  public void init()
+  {
+    
+    /**
+     * Steps:
+     *  -Set up buttons (Start, lore, exit)
+     *  -Get background forest.jpeg (background) and Starting_Screen_Danny.png
+     *  -Blur forest.jpeg
+     *  -add background and then starting screen
+     *  -add buttons
+     */
+    
+    
+   	JButton startButton = new JButton(START);
+   	startButton.addActionListener(this);
+   	JButton loreButton = new JButton(LORE);
+   	loreButton.addActionListener(this);
+   	JButton exitButton = new JButton(EXIT);
+   	exitButton.addActionListener(this);
+
+    // TODO Auto-generated method stub
+    
   }
 
   @Override
-  public void init()
+  public void actionPerformed(ActionEvent evt)
   {
-    // TODO Auto-generated method stub
+    /**
+     * Steps:
+     * 
+     */
+    String buttonName = evt.getActionCommand();
+    /**
+     * Start -> Send signal to LumbearjackGame class to change screens
+     * Lore -> Popup Jframe with Lore
+     * Exit -> Send signal to LumbearjackGame class to exit
+     */
+    
     
   }
 
